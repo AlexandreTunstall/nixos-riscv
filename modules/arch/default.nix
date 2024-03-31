724448dba476585b466052c2b909ca307a6cf017
Alex Tunstall <alex@tunstall.xyz>
2024-03-31 12:50:45 +0100

    Add a comment about mold's trade-offs

--- modules/arch/default.nix
+++ modules/arch/default.nix
@@ -3,7 +3,7 @@
   nixpkgs = {
     hostPlatform = {
       system = "riscv64-linux";
-      
+
       gcc = {
         arch = "rv64gc_zba_zbb";
         tune = "sifive-u74";
@@ -11,6 +11,8 @@
     };
 
     config = {
+      # Using mold might not be worth it, because it requires building 80+ more
+      # packages just for the stdenv. On the other hand, mold is much faster.
       replaceStdenv = { pkgs }: pkgs.useMoldLinker pkgs.gcc13Stdenv;
     };
   };
