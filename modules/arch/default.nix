4c4206dbe939a6c3fa150c8a8def6a36d07fb282
Alex Tunstall <alex@tunstall.xyz>
2024-04-02 12:04:39 +0100

    Revert back to using BFD ld
    It is painfully slow, but:
     - Mold doesn't work with GHC.
     - LLD is unusable in stdenv (too many packages break when using
       clang+LLD or when using GCC+LLD).
     - Gold is deprecated, bug-prone, and doesn't offer that much more
       performance than BFD.

--- modules/arch/default.nix
+++ modules/arch/default.nix
@@ -9,11 +9,5 @@
         tune = "sifive-u74";
       };
     };
-
-    config = {
-      # Using mold might not be worth it, because it requires building 80+ more
-      # packages just for the stdenv. On the other hand, mold is much faster.
-      replaceStdenv = { pkgs }: pkgs.useMoldLinker pkgs.gcc13Stdenv;
-    };
   };
 }
