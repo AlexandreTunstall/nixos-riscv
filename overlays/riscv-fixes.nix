28c3c90ff7d3fba240a492ee1d5f976ee90bdb5a
Alex Tunstall <alex@tunstall.xyz>
2024-04-02 16:53:32 +0100

    Fix catch2 failing to build due to alignment

--- overlays/riscv-fixes.nix
+++ overlays/riscv-fixes.nix
@@ -4,6 +4,12 @@ self: super: let
   });
 
 in {
+  # See https://github.com/catchorg/Catch2/issues/2808
+  # Fixed by https://github.com/NixOS/nixpkgs/pull/295243
+  catch2_3 = super.catch2_3.overrideAttrs ({ NIX_CFLAGS_COMPILE ? "", ... }: {
+    NIX_CFLAGS_COMPILE = "${NIX_CFLAGS_COMPILE} -Wno-error=cast-align";
+  });
+
   libbsd = dontCheck super.libbsd;
   libuv = dontCheck super.libuv;
   pixman = dontCheck super.pixman;
