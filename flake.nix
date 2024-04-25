554c80cc1e3f02b5a2f943b0c2c3cd426bb284c4
Alex Tunstall <alex@tunstall.xyz>
2024-04-25 06:08:58 +0100

    Add an all-haskell package to build all of Haskell
    This link farm excludes all packages that are known not to build on x86
    or have foreign dependencies that do not support RISC-V.
    It also excludes non-default package versions.

--- flake.nix
+++ flake.nix
@@ -35,11 +35,45 @@
       };
     };
 
-    packages.x86_64-linux = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      boot = pkgs.linkFarm "boot" {
-        "ghc-9.4.8" = nixpkgs-ghc-cross.legacyPackages.x86_64-linux.pkgsCross.riscv64.haskell.compiler.native-bignum.ghc948;
+    packages = {
+      x86_64-linux = let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in {
+        boot = pkgs.linkFarm "boot" {
+          "ghc-9.4.8" = nixpkgs-ghc-cross.legacyPackages.x86_64-linux.pkgsCross.riscv64.haskell.compiler.native-bignum.ghc948;
+        };
+      };
+
+      riscv64-linux = let
+        inherit (self.nixosConfigurations.nixos) pkgs;
+
+        usableMap = pkgs.lib.mapAttrs isUsable pkgs.haskellPackages // {
+          # Needs cplex (unfree)
+          cplex-hs = false;
+          # Needs folly (unsupported)
+          cabal2nix-unstable = false;
+          # Needs imagemagick (insecure)
+          HDRUtils = false;
+          # Needs luajit (unsupported)
+          gegl = false;
+          # Needs mplayer (unsupported)
+          mplayer-spot = false;
+          # Needs nvidia-x11 (unfree)
+          cuda = false;
+          # Needs openjdk (unsuppported)
+          hzk = false;
+          # Needs valgrind (unsupported)
+          hgdal = false;
+        };
+
+        isDerivation = v: builtins.isAttrs v && (v.type or "") == "derivation";
+        isUsable = name: drv: isDerivation drv && name == drv.pname
+          && drv.meta.available && !drv.meta.broken && !drv.meta.unfree
+          && pkgs.lib.all lookupUsable (drv.getBuildInputs.isHaskellPartition.right or []);
+        lookupUsable = drv: drv != null && usableMap.${drv.pname};
+      in {
+        all-haskell = pkgs.linkFarm "all-haskell"
+          (pkgs.lib.filterAttrs (name: _: usableMap.${name}) pkgs.haskellPackages);
       };
     };
   };
